b9773f5636ed8e9ef00529bb733a3eea2eac1145
Mayumi Yamanishi <s1190217@gmail.com>
2020-06-06 13:37:42 +0900

    Initial Commit

---
+++ CrouwdWorks_ChatAppTests/CrouwdWorks_ChatAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CrouwdWorks_ChatAppTests.swift
+//  CrouwdWorks_ChatAppTests
+//
+//  Created by m.yamanishi on 2020/06/06.
+//  Copyright © 2020 mayumi yamanishi. All rights reserved.
+//
+
+import XCTest
+@testable import CrouwdWorks_ChatApp
+
+class CrouwdWorks_ChatAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
